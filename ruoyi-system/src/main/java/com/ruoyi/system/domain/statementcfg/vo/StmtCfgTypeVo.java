8da15ea966b0073f5d043ba34d5b25e15ca3206f
LingZed <lingzed@163.com>
2025-07-01 10:20:33 +0800

    添加报表类型vo、报表配置类型vo
    Signed-off-by: LingZed <lingzed@163.com>

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/statementcfg/vo/StmtCfgTypeVo.java
@@ -0,0 +1,25 @@
+package com.ruoyi.system.domain.statementcfg.vo;
+
+/**
+ * 报表配置项类型vo
+ */
+public class StmtCfgTypeVo {
+    private Short typeId;
+    private String typeName;
+
+    public Short getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Short typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+}
---
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/statementcfg/vo/StmtTypeVo.java
@@ -0,0 +1,25 @@
+package com.ruoyi.system.domain.statementcfg.vo;
+
+/**
+ * 报表类型vo
+ */
+public class StmtTypeVo {
+    private String stmtCode;
+    private String stmtName;
+
+    public String getStmtCode() {
+        return stmtCode;
+    }
+
+    public void setStmtCode(String stmtCode) {
+        this.stmtCode = stmtCode;
+    }
+
+    public String getStmtName() {
+        return stmtName;
+    }
+
+    public void setStmtName(String stmtName) {
+        this.stmtName = stmtName;
+    }
+}
