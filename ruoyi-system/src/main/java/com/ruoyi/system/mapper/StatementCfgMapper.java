b6bdfe3062769a179440409485a565329eede943
LingZed <lingzed@163.com>
2025-07-01 09:55:30 +0800

    改动sql

--- ruoyi-system/src/main/java/com/ruoyi/system/mapper/StatementCfgMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/StatementCfgMapper.java
@@ -1,77 +1,93 @@
 package com.ruoyi.system.mapper;
 
 import com.ruoyi.system.domain.statementcfg.StatementCfg;
+import com.ruoyi.system.domain.statementcfg.dto.StatementCfgDto;
 import com.ruoyi.system.domain.statementcfg.query.StatementCfgQuery;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface StatementCfgMapper {
-    /** 
+    /**
      * 条件查询
-     * 
+     *
      * @param statementCfgQuery
      */
     List<StatementCfg> selectByCondition(StatementCfgQuery statementCfgQuery);
 
     /**
      * 条件计数
-     * 
+     *
      * @param statementCfgQuery
      */
     Long countByCondition(StatementCfgQuery statementCfgQuery);
 
-    /** 
+    /**
      * 查询所有
-     * 
+     *
      */
     List<StatementCfg> selectAll();
 
     /**
      * 计数所有
-     * 
+     *
      */
     Long countAll();
 
-    /** 
+    /**
      * 通过id查询
-     * 
+     *
      * @param id
      */
     StatementCfg selectById(Integer id);
 
-    /** 
-     * 添加
-     * 
-     * @param statementCfg
+    /**
+     * 通过cfg_code查询
+     * @param cfgCode
+     * @return
      */
-    void insert(StatementCfg statementCfg);
+    StatementCfg selectByCfgCode(String cfgCode);
 
-    /** 
+    /**
+     * 通过id列表查询
+     * @param idList
+     * @return
+     */
+    List<StatementCfg> selectByIds(@Param("idList") List<Integer> idList);
+
+    /**
+     * 添加
+     *
+     * @param statementCfgDto
+     */
+    void insert(StatementCfgDto statementCfgDto);
+
+    /**
      * 批量添加
-     * 
+     *
      * @param list
      */
     void batchInsert(List<StatementCfg> list);
 
-    /** 
+    /**
      * 编辑
-     * 
-     * @param statementCfg
+     *
+     * @param statementCfgDto
      */
-    void update(StatementCfg statementCfg);
+    Integer update(StatementCfgDto statementCfgDto);
 
-    /** 
+    /**
      * 批量编辑
-     * 
+     *
      * @param list
      */
     void batchUpdate(List<StatementCfg> list);
 
-    /** 
+    /**
      * 删除
-     * 
+     *
      * @param list
      */
     void delete(List<Integer> list);
