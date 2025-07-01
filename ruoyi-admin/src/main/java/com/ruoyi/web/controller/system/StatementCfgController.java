46db46d231196a0044b5985807906fd3ddf9b3c4
LingZed <lingzed@163.com>
2025-07-01 10:19:06 +0800

    添加报表配置控制器
    Signed-off-by: LingZed <lingzed@163.com>

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/StatementCfgController.java
@@ -0,0 +1,180 @@
+package com.ruoyi.web.controller.system;
+
+import com.ruoyi.common.constant.MsgConstants;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.common.core.redis.RedisCache;
+import com.ruoyi.common.enums.statement.StatementCfgType;
+import com.ruoyi.common.enums.statement.StatementType;
+import com.ruoyi.common.exception.ServiceException;
+import com.ruoyi.common.utils.StmtCfgUtil;
+import com.ruoyi.common.validated.group.Add;
+import com.ruoyi.common.validated.group.Edit;
+import com.ruoyi.system.domain.statementcfg.RowColHeadIndexCfg;
+import com.ruoyi.system.domain.statementcfg.StatementCfg;
+import com.ruoyi.system.domain.statementcfg.dto.StatementCfgDto;
+import com.ruoyi.system.domain.statementcfg.query.StatementCfgQuery;
+import com.ruoyi.system.domain.statementcfg.vo.StatementCfgVo;
+import com.ruoyi.system.domain.statementcfg.vo.StmtCfgTypeVo;
+import com.ruoyi.system.domain.statementcfg.vo.StmtTypeVo;
+import com.ruoyi.system.service.StatementCfgService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * 报表配置控制器
+ */
+@RestController
+@RequestMapping("/stmt-cfg")
+public class StatementCfgController extends BaseController {
+    private static final Logger log = LoggerFactory.getLogger(StatementCfgController.class);
+
+    @Resource
+    private StatementCfgService statementCfgService;
+    @Resource
+    private RedisCache redisCache;
+
+    /**
+     * 加载报表配置列表
+     * @param corpCode
+     * @param statementName
+     * @param cfgType
+     * @return
+     */
+    @PreAuthorize("@ss.hasPermi('system:config:query')")
+    @GetMapping
+    public TableDataInfo loadCfgList(String corpCode, String statementName, Short cfgType) {
+        startPage();
+        StatementCfgQuery query = new StatementCfgQuery();
+        query.setCorpCode(corpCode);
+        query.setStatementName(statementName);
+        query.setCfgType(cfgType);
+        List<StatementCfgVo> voList = statementCfgService.findVoListByCondition(query);
+        return getDataTable(voList);
+    }
+
+    /**
+     * 获取报表配置
+     * @param corpCode
+     * @return
+     */
+    @PreAuthorize("@ss.hasPermi('system:config:query')")
+    @GetMapping("/cache")
+    public AjaxResult getStmtCfg(String corpCode) {
+        String key = StmtCfgUtil.getCfgCode(StatementType.GGS_GSF_SR_QK_TJB, StatementCfgType.ROW_COL_HEAD_INDEX_CFG, corpCode);
+        List<RowColHeadIndexCfg> cfgCacheList = statementCfgService.getListStmtCfgCache(key, RowColHeadIndexCfg.class);
+        log.info("cache: {}", cfgCacheList);
+        return success(cfgCacheList);
+    }
+
+    /**
+     * 添加报表配置
+     * @param statementCfgDto
+     * @return
+     */
+    @PostMapping
+    public AjaxResult addStmtCfg(@Validated(Add.class) @RequestBody StatementCfgDto statementCfgDto) {
+        statementCfgService.addStmtCfg(statementCfgDto);
+        return success();
+    }
+
+    /**
+     * 更新报表配置
+     * @param statementCfgDto
+     * @return
+     */
+    @PutMapping
+    public AjaxResult editStmtCfg(@Validated(Edit.class) @RequestBody StatementCfgDto statementCfgDto) {
+        statementCfgService.editStmtCfg(statementCfgDto);
+        return success();
+    }
+
+    /**
+     * 删除报表配置
+     * @param idList
+     * @return
+     */
+    @DeleteMapping("/{idList}")
+    public AjaxResult delStmtCfg(@PathVariable List<Integer> idList) {
+        statementCfgService.delStmtCfg(idList);
+        return success();
+    }
+
+    /**
+     * 加载报表配置的类型列表
+     * @return
+     */
+    @GetMapping("/types")
+    public AjaxResult loadStmtCfgTypeList() {
+        StatementCfgType[] values = StatementCfgType.values();
+        List<StmtCfgTypeVo> typeList = Stream.of(values)
+                .map(type -> {
+                    StmtCfgTypeVo vo = new StmtCfgTypeVo();
+                    vo.setTypeId(type.getCfgType());
+                    vo.setTypeName(type.getTypeName());
+                    return vo;
+                }).collect(Collectors.toList());
+        return success(typeList);
+    }
+
+    /**
+     * 加载报表类型列表
+     * @return
+     */
+    @GetMapping("/stmts")
+    public AjaxResult loadStmtTypeList() {
+        StatementType[] values = StatementType.values();
+        List<StmtTypeVo> typeList = Stream.of(values)
+                .map(type -> {
+                    StmtTypeVo vo = new StmtTypeVo();
+                    vo.setStmtCode(type.getStatementCode());
+                    vo.setStmtName(type.getStatementName());
+                    return vo;
+                }).collect(Collectors.toList());
+        return success(typeList);
+    }
+
+    /**
+     * 对照缓存与数据库是否一致
+     * @return
+     */
+    @GetMapping("contrast/{cfgCode}")
+    public AjaxResult contrastDbAndCache(@PathVariable String cfgCode) {
+        StatementCfg byCfgCode = statementCfgService.findByCfgCode(cfgCode);
+        Map<String, Boolean> res = new HashMap<>();
+        if (byCfgCode == null) {
+            res.put("isConsistent", false);
+            return success(res);
+        }
+        String dbContent = byCfgCode.getCfgContent();
+        String cache = redisCache.getCacheStr(cfgCode);
+        res.put("isConsistent", dbContent.equals(cache));
+        return success(res);
+    }
+
+    /**
+     * 手动同步缓存
+     * @param cfgCode
+     * @return
+     */
+    @GetMapping("synchronized/{cfgCode}")
+    public AjaxResult synchronizedCache(@PathVariable String cfgCode) {
+        StatementCfg byCfgCode = statementCfgService.findByCfgCode(cfgCode);
+        if (byCfgCode == null){
+            throw new ServiceException(MsgConstants.STMT_CFG_NOT_EXISTS);
+        }
+        redisCache.setCacheStr(cfgCode, byCfgCode.getCfgContent());
+        return success();
+    }
+}
---
+++ ruoyi-common/src/main/java/com/ruoyi/common/constant/MsgConstants.java
@@ -0,0 +1,27 @@
+package com.ruoyi.common.constant;
+
+/**
+ * 消息常量
+ */
+public class MsgConstants {
+    public static final String STMT_CFG_CONTENT_IS_EMPTY = "报表配置的内容为空";
+    public static final String STMT_CFG_NOT_EXISTS = "报表配置不存在";
+    public static final String STMT_CFG_CACHE_CONTENT_IS_EMPTY = "报表配置: %s 缓存的内容为空";
+    public static final String UNKNOWN_STMT_CFG_TYPE_V1 = "未知的报表配置类型";
+    public static final String UNKNOWN_STMT_CFG_TYPE = UNKNOWN_STMT_CFG_TYPE_V1 + ": %d";
+    public static final String STMT_CFG_CODE_EXIST = "配置项编码已存在: %s";
+    public static final String CORP_CODE_REQUIRED = "公司编码不能为空";
+    public static final String STMT_CODE_REQUIRED = "报表编码不能为空";
+    public static final String STMT_CODE_NOT_CHINESE = "报表编码不能包含中文";
+    public static final String STMT_NAME_REQUIRED = "报表名称不能为空";
+    public static final String CFG_CONT_REQUIRED = "报表的配置内容不能为空";
+    public static final String CFG_CONT_TYPE_REQUIRED = "配置内容的Json类型不能为空";
+    public static final String JSON_CONT_NOT_LEGAL_FORMAT = "不是合法的Json格式";
+    public static final String CFG_CONT_NOT_LEGAL_FORMAT = "配置内容" + JSON_CONT_NOT_LEGAL_FORMAT;
+    public static final String UNKNOWN_J_TYPE = "未知json类型";
+    public static final String CFG_TYPE_REQUIRED = "配置类型不能为空";
+    public static final String ID_REQUIRED = "ID不能为空";
+    public static final String ERROR_ID = "错误ID";
+    public static final String ID_NOT_EXIST = "ID【%s】不存在";
+    public static final String UPDATE_FAILED = "更新失败，请重试";
+}
