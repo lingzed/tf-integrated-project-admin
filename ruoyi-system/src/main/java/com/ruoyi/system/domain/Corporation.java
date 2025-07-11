19c5d4a544fa9538a056965ed5dc63d20eeb6f4e
LingZed <lingzed@163.com>
2025-07-11 09:48:54 +0800

    公司表实体
    Signed-off-by: LingZed <lingzed@163.com>

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/Corporation.java
@@ -0,0 +1,43 @@
+package com.ruoyi.system.domain;
+
+/**
+ * 公司表实体
+ */
+public class Corporation {
+    private Integer id;     // id
+    private String corpCode;    // 公司编码
+    private String pCorpCode;   // 公司上级编码
+    private String corpName;    // 公司名称
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCorpCode() {
+        return corpCode;
+    }
+
+    public void setCorpCode(String corpCode) {
+        this.corpCode = corpCode;
+    }
+
+    public String getpCorpCode() {
+        return pCorpCode;
+    }
+
+    public void setpCorpCode(String pCorpCode) {
+        this.pCorpCode = pCorpCode;
+    }
+
+    public String getCorpName() {
+        return corpName;
+    }
+
+    public void setCorpName(String corpName) {
+        this.corpName = corpName;
+    }
+}
