3fb3104867debec730b548616b26a0d35b70b466
LingZed <lingzed@163.com>
2025-07-11 09:46:23 +0800

    辅助核算项目实体
    Signed-off-by: LingZed <lingzed@163.com>

---
+++ ruoyi-common/src/main/java/com/ruoyi/common/u8c/AssVo.java
@@ -0,0 +1,33 @@
+package com.ruoyi.common.u8c;
+/**
+ * 辅助核算
+ */
+public class AssVo {
+    private String checktypecode;   // 辅助核算类型编码
+    private String checkvaluecode;  // 辅助核算的值的编码
+    private String checkvaluename;  // 辅助核算的值的名称
+
+    public String getChecktypecode() {
+        return checktypecode;
+    }
+
+    public void setChecktypecode(String checktypecode) {
+        this.checktypecode = checktypecode;
+    }
+
+    public String getCheckvaluecode() {
+        return checkvaluecode;
+    }
+
+    public void setCheckvaluecode(String checkvaluecode) {
+        this.checkvaluecode = checkvaluecode;
+    }
+
+    public String getCheckvaluename() {
+        return checkvaluename;
+    }
+
+    public void setCheckvaluename(String checkvaluename) {
+        this.checkvaluename = checkvaluename;
+    }
+}
